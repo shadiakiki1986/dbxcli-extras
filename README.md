7a6cd787831c72d7d2b65971c475e639dec71c4b
Shadi Akiki <shadiakiki1986@gmail.com>
2021-07-23 15:17:47 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+## dbxcli-sync
+
+dbxcli sync command that solves https://github.com/dropbox/dbxcli/issues/53
+
+
+## Install
+
+```
+pip3 install dbxcli_sync
+```
+
+## Usage
+
+```
+dbxcli_sync [--verbose={0,1,2}] <localdir> <dbxdir>
+```
+
+
+## Dev notes
+
+```
+pip3 install pew
+pew new dbxcli_sync
+pip3 install -e .
+```
---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+  
+# copied from https://github.com/awslabs/git-remote-codecommit/blob/master/setup.py
+import os
+def read(fname):
+  return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+# follow https://github.com/awslabs/git-remote-codecommit/blob/master/setup.py
+# and https://packaging.python.org/tutorials/packaging-projects/
+setup(
+    name='dbxcli_sync',
+    version="0.1",
+    author="Shadi Akiki",
+    author_email="shadi.akiki@ronininstitute.org",
+    url='https://www.teamshadi.net',
+    description="dbxcli sync command that solves https://github.com/dropbox/dbxcli/issues/53",
+    long_description = 'dbxcli sync command that solves https://github.com/dropbox/dbxcli/issues/53',
+    long_description_content_type="text/markdown",
+
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+      "click==7.1.2",
+      "tqdm==4.56.0",
+    ],
+    entry_points='''
+        [console_scripts]
+        dbxcli_sync=dbxcli_sync.cli:cli
+    ''',
+)
+
