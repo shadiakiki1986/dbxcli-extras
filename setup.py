c4c87d0e4c62a1c4c31ba8fa2fa0e1c792480f91
Shadi Akiki <shadiakiki1986@gmail.com>
2021-10-06 15:35:23 -0400

    update version in setup.py to 0.0.4

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 # and https://packaging.python.org/tutorials/packaging-projects/
 setup(
     name='dbxcli_extras',
-    version="0.0.3",
+    version="0.0.4",
     license="WTFPL",
     author="Shadi Akiki",
     author_email="shadi.akiki@ronininstitute.org",
